ccc05a1f991a8655423f48a872095c10c07cf9cf
PeterJ504 <you@example.com>
2019-08-04 20:17:26 +0800

    code cleanup

--- deck.py
+++ deck.py
@@ -2,49 +2,60 @@ import cards
 import random
 from operator import attrgetter
 
+
 class Deck():
 
     def __init__(self):
-        self.cards=[]
+        self.cards = []
         self.create()
-            
+
     def create(self):
-        suits = ['s','h','d','c']
-        numbers = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-        self.cards = [ cards.Card(n, s) for n in numbers for s in suits  ]
+        suits = ['s', 'h', 'd', 'c']
+        numbers = ['A', 'K', 'Q', 'J', 'T', '9',
+                   '8', '7', '6', '5', '4', '3', '2']
+        self.cards = [cards.Card(n, s) for n in numbers for s in suits]
 
     @property
     def shuffleCards(self):
         return random.shuffle(self.cards)
-    
+
     @property
     def printCards(self):
         for card in self.cards:
-            print(card,end=" ")
+            print(card, end=" ")
         print()
 
-    def e_sort(self,cards):
-        return Card.num
-
-
-    # @property
+    @property
     def sortCards(self):
         order = "AKQJT98765432"
         order2 = "shdc"
-        s_cards = sorted(self.cards, key=lambda Card: 
-            (order.index(Card.num),order2.index(Card.suit)))
-        self.cards=s_cards
+        s_cards = sorted(self.cards, key=lambda Card:
+                         (order.index(Card.num), order2.index(Card.suit)))
+        self.cards = s_cards
+
+
+class Hand(Deck):
+
+    def __init__(self):
+        # handFolded = True
+        self.cards = []
+        self.deck = Deck
+
+    def addHand(self, Deck):
+        self.cards = []
+
+    def addCards(self):
+        pass
+
 
 def main():
-    myDeck=Deck()
+    myDeck = Deck()
     print(len(myDeck.cards))
-    # myDeck.printCards
+    myDeck.printCards
     myDeck.shuffleCards
     myDeck.printCards
-    sorted_cards = myDeck.sortCards()
+    myDeck.sortCards
     myDeck.printCards
-
-    
 
 
 if __name__ == "__main__":
